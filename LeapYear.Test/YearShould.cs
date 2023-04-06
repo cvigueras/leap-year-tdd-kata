d070738246339aef3e2b128ac0f8b813a913e8fe
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-04-06 17:19:08 +0200

    Refactor: extract class in console test. Grouped test in testcases.

--- LeapYear.Test/YearShould.cs
+++ LeapYear.Test/YearShould.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using LeapYear.Console;
 
 namespace LeapYear.Test
 {
@@ -9,48 +10,14 @@ namespace LeapYear.Test
         {
         }
 
-        [Test]
-        public void return_true_if_is_leap_year()
+        [TestCase(2000)]
+        [TestCase(800)]
+        [TestCase(400)]
+        public void return_true_if_is_leap_year(int year)
         {
-            var result = Year.IsLeapYear(2000);
+            var result = Year.IsLeapYear(year);
 
             result.Should().Be(true);
-        }
-
-        [Test]
-        public void return_true_if_is_leap_year_other_year()
-        {
-            var result = Year.IsLeapYear(800);
-
-            result.Should().Be(true);
-        }
-
-        [Test]
-        public void return_true_if_is_leap_year_other_year_more()
-        {
-            var result = Year.IsLeapYear(400);
-
-            result.Should().Be(true);
-        }
-    }
-
-    public class Year
-    {
-        public static bool IsLeapYear(int year)
-        {
-            if(year == 2000)
-            {
-                return true;
-            }
-            if(year == 800)
-            {
-                return true;
-            }
-            if(year == 400)
-            {
-                return true;
-            }
-            return false;
         }
     }
 }
