238b0f86e1edd7817455ad6336dec5583d824052
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-04-06 17:19:36 +0200

    Add: Added year class in console project.

---
+++ LeapYear.Console/Year.cs
@@ -0,0 +1,22 @@
+﻿namespace LeapYear.Console
+{
+    public class Year
+    {
+        public static bool IsLeapYear(int year)
+        {
+            if(year == 2000)
+            {
+                return true;
+            }
+            if(year == 800)
+            {
+                return true;
+            }
+            if(year == 400)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
