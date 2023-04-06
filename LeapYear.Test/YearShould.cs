6c34f508b3d0c3aa0def7b92075718070196de10
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-04-06 17:41:14 +0200

    Refactor: grouped test in testcases.

--- LeapYear.Test/YearShould.cs
+++ LeapYear.Test/YearShould.cs
@@ -30,26 +30,12 @@ namespace LeapYear.Test
             result.Should().Be(false);
         }
 
-        [Test]
-        public void return_true_if_is_divisible_by_4_not_divisible_by_100()
+        [TestCase(2008)]
+        [TestCase(2012)]
+        [TestCase(2016)]
+        public void return_true_if_is_divisible_by_4_not_divisible_by_100(int year)
         {
-            var result = Year.IsLeapYear(2008);
-
-            result.Should().Be(true);
-        }
-
-        [Test]
-        public void return_true_if_is_divisible_by_4_not_divisible_by_100_other_number()
-        {
-            var result = Year.IsLeapYear(2012);
-
-            result.Should().Be(true);
-        }
-
-        [Test]
-        public void return_true_if_is_divisible_by_4_not_divisible_by_100_other_number_more()
-        {
-            var result = Year.IsLeapYear(2016);
+            var result = Year.IsLeapYear(year);
 
             result.Should().Be(true);
         }
