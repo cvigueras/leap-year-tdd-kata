9904887013e449cb4f7590df8d6d7fe091fe0c1c
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-04-06 17:12:28 +0200

    Test: return_true_if_is_leap_year

--- LeapYear.Test/YearShould.cs
+++ LeapYear.Test/YearShould.cs
@@ -1,5 +1,4 @@
 using FluentAssertions;
-using LeapYear.Console;
 
 namespace LeapYear.Test
 {
@@ -8,6 +7,23 @@ namespace LeapYear.Test
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public void return_true_if_is_leap_year()
+        {
+            var result = Year.IsLeapYear(2000);
+
+            result.Should().Be(true);
+        }
+        
+    }
+
+    public class Year
+    {
+        public static bool IsLeapYear(int year)
+        {
+            throw new NotImplementedException();
         }
     }
 }
