283b4cf58bf5c460a686fe83c31c2d98741cce27
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-04-06 17:06:46 +0200

    Chore: Init project.

---
+++ LeapYear.Test/YearShould.cs
@@ -0,0 +1,13 @@
+using FluentAssertions;
+using LeapYear.Console;
+
+namespace LeapYear.Test
+{
+    public class YearShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
