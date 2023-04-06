1b7f3c27af36f8b0b28e5d77482da846072cadbe
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-04-06 17:36:30 +0200

    Test: return_true_if_is_divisible_by_4_not_divisible_by_100_other_number

--- LeapYear.Test/YearShould.cs
+++ LeapYear.Test/YearShould.cs
@@ -37,5 +37,13 @@ namespace LeapYear.Test
 
             result.Should().Be(true);
         }
+
+        [Test]
+        public void return_true_if_is_divisible_by_4_not_divisible_by_100_other_number()
+        {
+            var result = Year.IsLeapYear(2012);
+
+            result.Should().Be(true);
+        }
     }
 }
