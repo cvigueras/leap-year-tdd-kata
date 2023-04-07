87d8adf78fe9c83238b47ac8d16b0c8e58f798dc
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-04-07 13:14:35 +0200

    Refactor: grouped if statement.

--- LeapYear.Console/Year.cs
+++ LeapYear.Console/Year.cs
@@ -4,23 +4,9 @@
     {
         public static bool IsLeapYear(int year)
         {
-            if(year % 400 == 0)
-            {
-                return true;
-            }
-            if(year % 100 == 0 && year % 400 != 0)
-            {
-                return false;
-            }
-            if (year % 4 == 0 && year % 100 != 0)
-            {
-                return true;
-            }
-            if (year % 4 != 0)
-            {
-                return false;
-            }
-            return false;
+            return year != 0 
+                && (year % 400 == 0 || year % 4 == 0 
+                && year % 100 > 0);
         }
     }
 }
